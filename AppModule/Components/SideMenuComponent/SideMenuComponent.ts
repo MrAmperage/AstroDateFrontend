7f30fc88159b845c7e6ab82997c7ef60e37d6052
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-10-17 15:18:39 +0700

    Update to 0.0.0.1b

---
+++ AppModule/Components/SideMenuComponent/SideMenuComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SideMenu',
+  templateUrl: './SideMenuComponent.html',
+  styleUrls: ['./SideMenuComponent.sass'],
+})
+export class SideMenu {}

47a655e1dc8718d1c2127af8b1d2756b69f461b4
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-10-18 14:21:47 +0700

    Update to 0.0.0.1e

--- AppModule/AppModule.ts
+++ AppModule/AppModule.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './AppModuleRouting';
 import { AppComponent } from './Components/AppComponent/AppComponent';
 import { SideMenuComponent } from './Components/SideMenuComponent/SideMenuComponent';
+import { HeaderComponent } from './Components/HeaderComponent/HeaderComponent';
 
 @NgModule({
-  declarations: [AppComponent, SideMenuComponent],
+  declarations: [AppComponent, SideMenuComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ AppModule/Components/HeaderComponent/HeaderComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Header',
+  templateUrl: './HeaderComponent.html',
+  styleUrls: ['./HeaderComponent.sass'],
+})
+export class HeaderComponent {}
